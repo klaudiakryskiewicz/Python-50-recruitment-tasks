31ce84a7a247f8f3de8ae75dccaba08d3d1bdf96
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-08-09 12:27:45 +0200

    17 done

---
+++ 17.py
@@ -0,0 +1,8 @@
+"""Znajdź różnicę między największą a najmniejszą
+wartością na poniższej liście.
+Zadbaj o to, aby rozwiązanie było efektywne."""
+
+A = [4, 5, 7, -3, 2, 8, -10, 15]
+result = max(A) - min(A)
+
+print(result)
