1ee3ab1e3990ee76166d0d951cae6de7c93b78d9
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-08-09 12:04:13 +0200

    14 done

---
+++ 14.py
@@ -0,0 +1,9 @@
+"""Wypisz pierwsze 5 elementów listy L.
+Wypisz co drugą literę stringa s, zaczynając od ostatniej i
+cofając się do początku.
+"""
+L = [11, 22, 33, 44, 55, 66, 77, 88, 99, 1010]
+s = 'a nMozh^tKysPW 9ęxi b$uML'
+
+print(L[:5])
+print(s[-1::-2])
