0f9557211909e22225fb37b6f6938994a96693d4
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-08-15 23:00:10 +0200

    30 done

---
+++ 30.py
@@ -0,0 +1,5 @@
+"""Jaką wartość przyjmie poniższe zdanie logiczne?
+Wyjaśnij proces jego ewaluacji i znaczenie
+poszczególnych słów: is, not, in.
+"""
+print(1 is not True in [1,2,3]) #(A is B) and (B in C)
